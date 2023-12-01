b02588d300cde894c50ceab6d5ac5bdc6082e917
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2023-12-01 08:47:43 -0300

    Scrollable ScrollPhysics

--- lib/views/ui/onboarding/onboarding_screen.dart
+++ lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:job/controllers/exports.dart';
 import 'package:job/views/common/exports.dart';
 import 'package:job/views/ui/onboarding/widgets/page_one.dart';
 import 'package:job/views/ui/onboarding/widgets/page_three.dart';
 import 'package:job/views/ui/onboarding/widgets/page_two.dart';
+import 'package:provider/provider.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        PageView(
+      body:
+          Consumer<OnBoardNotifier>(builder: (context, onBoardNotifier, child) {
+        return Stack(
           children: [
-            PageOne(),
-            PageTwo(),
-            PageThree(),
+            PageView(
+              physics: onBoardNotifier.isLastPage
+                  ? const NeverScrollableScrollPhysics()
+                  : const AlwaysScrollableScrollPhysics(),
+              controller: pageController,
+              onPageChanged: (page) {
+                onBoardNotifier.isLastPage = page == 2;
+              },
+              children: const [
+                PageOne(),
+                PageTwo(),
+                PageThree(),
+              ],
+            ),
           ],
-        )
-      ],
-    ));
+        );
+      }),
+    );
   }
 }
