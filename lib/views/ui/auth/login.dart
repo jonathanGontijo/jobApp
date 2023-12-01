d5121a2b112714a9c9e4ed60dc5e3ee9d66c341f
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2023-12-01 17:33:26 -0300

    appbar Login Page

--- lib/views/ui/auth/login.dart
+++ lib/views/ui/auth/login.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:job/constants/app_constants.dart';
+import 'package:job/views/common/app_bar.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -22,6 +25,18 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: CustomAppBar(
+          text: "Login",
+          child: GestureDetector(
+            onTap: () {
+              Get.back();
+            },
+            child: const Icon(CupertinoIcons.arrow_left),
+          ),
+        ),
+      ),
       body: Container(
         color: Color(kOrange.value),
       ),
